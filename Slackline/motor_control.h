b4da9770a29e89e4665324713d5d3dc7551a04e0
josquintille <57634434+josquintille@users.noreply.github.com>
2021-04-15 11:42:56 +0200

    motor_control.h

---
+++ Slackline/motor_control.h
@@ -0,0 +1,13 @@
+/*
+ * motor_control.h
+ *
+ *  Created on: 15 avr. 2021
+ *      Author: tille
+ */
+
+#ifndef MOTOR_CONTROL_H_
+#define MOTOR_CONTROL_H_
+
+void motor_control_start();
+
+#endif /* MOTOR_CONTROL_H_ */
