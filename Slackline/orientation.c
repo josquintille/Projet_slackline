79fc147903b30d072f637d94b26ad57eb899a4e7
josquintille <57634434+josquintille@users.noreply.github.com>
2021-04-20 10:30:11 +0200

    clean prepocessor

--- Slackline/orientation.c
+++ Slackline/orientation.c
@@ -16,11 +16,6 @@
 #endif
 
 #include <msgbus/messagebus.h>
-#include <hal.h>
-#include "msgbus/messagebus.h"
-#include "i2c_bus.h"
-#include "sensors/mpu9250.h"
-#include "sensors/mpu9250_registers.h"
 #include <sensors/imu.h>
 
 #include <math.h>
@@ -50,25 +45,13 @@ static THD_FUNCTION(orientation_thd, arg)
      while(1)
      {
     	 messagebus_topic_wait(imu_topic, &imu_values, sizeof(imu_values));
-		 //prints raw values
-		 chprintf((BaseSequentialStream *)&SD3, "%Ax=%-7d Ay=%-7d Az=%-7d Gx=%-7d Gy=%-7d Gz=%-7d\r\n",
-				 imu_values.acc_raw[X_AXIS], imu_values.acc_raw[Y_AXIS], imu_values.acc_raw[Z_AXIS],
-				 imu_values.gyro_raw[X_AXIS], imu_values.gyro_raw[Y_AXIS], imu_values.gyro_raw[Z_AXIS]);
-
-		 //prints raw values with offset correction
-		 chprintf((BaseSequentialStream *)&SD3, "%Ax=%-7d Ay=%-7d Az=%-7d Gx=%-7d Gy=%-7d Gz=%-7d\r\n",
-				 imu_values.acc_raw[X_AXIS]-imu_values.acc_offset[X_AXIS],
-				 imu_values.acc_raw[Y_AXIS]-imu_values.acc_offset[Y_AXIS],
-				 imu_values.acc_raw[Z_AXIS]-imu_values.acc_offset[Z_AXIS],
-				 imu_values.gyro_raw[X_AXIS]-imu_values.gyro_offset[X_AXIS],
-				 imu_values.gyro_raw[Y_AXIS]-imu_values.gyro_offset[Y_AXIS],
-				 imu_values.gyro_raw[Z_AXIS]-imu_values.gyro_offset[Z_AXIS]);
 
 		 //prints values in readable units
 		 chprintf((BaseSequentialStream *)&SD3, "%Ax=%.2f Ay=%.2f Az=%.2f Gx=%.2f Gy=%.2f Gz=%.2f (%x)\r\n\n",
 				 imu_values.acceleration[X_AXIS], imu_values.acceleration[Y_AXIS], imu_values.acceleration[Z_AXIS],
 				 imu_values.gyro_rate[X_AXIS], imu_values.gyro_rate[Y_AXIS], imu_values.gyro_rate[Z_AXIS],
 				 imu_values.status);
+		 chThdSleepMilliseconds(1000);
      }
 }
 void orientation_start(void)
@@ -126,7 +109,7 @@ static void timer11_start(void)
     gptStartContinuous(&GPTD11, 0xFFFF);
 }
 
-float get_angle()
+float get_angle(void)
 {
 	return angle;
 }
