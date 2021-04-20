960689e09736d3c78ba487bcc974090407494895
josquintille <57634434+josquintille@users.noreply.github.com>
2021-04-20 14:43:31 +0200

    angle form acceleration

--- Slackline/orientation.c
+++ Slackline/orientation.c
@@ -24,6 +24,11 @@
 #define AXIS_OF_ANGLE 	X_AXIS
 #define TIM2SEC(tim) 	tim/1e6
 
+#define STD_GRAVITY 		9.855f
+#define GRAVITY_DEVIATION	 0.12f
+#define GRAVITY_AXIS 	Y_AXIS
+
+#define THREAD_PERIODE 10 //[ms]
 // Bus to communicate with the IMU
 messagebus_t bus;
 MUTEX_DECL(bus_lock);
@@ -32,7 +37,9 @@ CONDVAR_DECL(bus_condvar);
 static float angle = 0;
 
 static void timer11_start(void);
-static void update_angle(float current_speed);
+static void update_angle_gyro(float current_speed);
+static bool is_device_stable(float acceleration[]);
+static void update_angle_acc(float acceleration[]);
 
 static THD_WORKING_AREA(orientation_thd_wa, 512);
 static THD_FUNCTION(orientation_thd, arg)
@@ -42,18 +49,24 @@ static THD_FUNCTION(orientation_thd, arg)
 
      messagebus_topic_t *imu_topic = messagebus_find_topic_blocking(&bus, "/imu");
      imu_msg_t imu_values;
+
+     systime_t time;
      while(1)
      {
+    	 time = chVTGetSystemTime();
     	 messagebus_topic_wait(imu_topic, &imu_values, sizeof(imu_values));
-    	 update_angle(imu_values.gyro_rate[AXIS_OF_ANGLE]);
+    	 //update_angle_gyro(imu_values.gyro_rate[AXIS_OF_ANGLE]);
+    	 update_angle_acc(imu_values.acceleration);
 
 		 //prints values in readable units
-		/* chprintf((BaseSequentialStream *)&SD3, "%Ax=%.2f Ay=%.2f Az=%.2f Gx=%.2f Gy=%.2f Gz=%.2f (%x)\r\n\n",
+		 /*chprintf((BaseSequentialStream *)&SD3, "%Ax=%.4f Ay=%.4f Az=%.4f Gx=%.4f Gy=%.4f Gz=%.4f (%x)\n\n",
 				 imu_values.acceleration[X_AXIS], imu_values.acceleration[Y_AXIS], imu_values.acceleration[Z_AXIS],
 				 imu_values.gyro_rate[X_AXIS], imu_values.gyro_rate[Y_AXIS], imu_values.gyro_rate[Z_AXIS],
 				 imu_values.status);*/
 		 chprintf((BaseSequentialStream *)&SD3, "%angle=%.4f\n",angle*180/3.141592653 );
-		 chThdSleepMilliseconds(10);
+
+		 // go to sleep
+		 chThdSleepUntilWindowed(time, time + MS2ST(THREAD_PERIODE));
      }
 }
 void orientation_start(void)
@@ -66,8 +79,8 @@ void orientation_start(void)
 	calibrate_acc();
 	calibrate_gyro();
 
-	// launch the thread
-	chThdCreateStatic(orientation_thd_wa, sizeof(orientation_thd_wa), NORMALPRIO, orientation_thd, NULL);
+	// launch the thread (priority +1 for the integrator)
+	chThdCreateStatic(orientation_thd_wa, sizeof(orientation_thd_wa), NORMALPRIO+1, orientation_thd, NULL);
 }
 
 
@@ -79,20 +92,34 @@ void orientation_start(void)
  * the angular velocity
  * 	input : angular_speed in the wanted direction [rad/s]
  */
-static void update_angle(float current_speed)
+static void update_angle_gyro(float current_speed)
 {
 	static float previous_speed = 0;
 
 	// calculate dt
 	chSysLock();
-	volatile uint16_t time = GPTD11.tim->CNT;
+	uint16_t time = GPTD11.tim->CNT;
 	// integrate with triangle methode
 	angle += 0.5*(previous_speed+current_speed)*TIM2SEC(time);
 	GPTD11.tim->CNT = 0;
-// modifeir : utiliser sleepUntilwindowd !!!
+	// modifeir : utiliser sleepUntilwindowd !!!
 	chSysUnlock();
 
 	previous_speed = current_speed;
+}
+static bool is_device_stable(float acceleration[])
+{
+	float module_sq = 0;
+	for(uint8_t i = 0; i<NB_AXIS;i++)
+	{
+		module_sq = acceleration[i]*acceleration[i];
+	}
+
+	return abs(module_sq-STD_GRAVITY*STD_GRAVITY) < GRAVITY_DEVIATION;
+}
+static void update_angle_acc(float acceleration[])
+{
+	angle = asin(acceleration[GRAVITY_AXIS]/STD_GRAVITY);
 }
 static void timer11_start(void)
 {
